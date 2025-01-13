e7f26c7839867b4d30c32b0aab038b09c0ce152d
Jostero32 <jostero32@gmail.com>
2025-01-13 01:59:18 -0500

    a

--- models/conexion.php
+++ models/conexion.php
@@ -1,10 +1,10 @@
 <?php
 class Conexion{
     public function conectar(){
-        $servername ="mysql-compuvisual";
-        $username = "root";
-        $password = "1234";
-        $db = "cuarto";
+        $servername ="mysql-iomk";
+        $username = "mysql";
+        $password = "Hrg52t4DDD6NhP/i12CgYV4Eg2l0YiTNt4W+vqZCSMA=";
+        $db = "mysql";
         $conn = mysqli_connect($servername,$username,$password,$db);
         if (!$conn) {
             echo"Error de conexion ".mysqli_connect_error();
